aab565a7db09191a6672524f7445cc377da6fe13
L3-INFO <fanny.miech@le-campus-numerique.fr>
2021-12-09 12:07:02 +0100

    last dance

--- src/get/devices.py
+++ src/get/devices.py
@@ -1,17 +1,34 @@
+import boto3
 import json
 import os
 
 # Get environment variable
 DEVICES_TABLE = os.environ['DEVICES_TABLE']
 
-def get_devices(context, event): 
+def get_devices(context, event):
+    print("")
     print(DEVICES_TABLE)
-    return { 
-        "statusCode": 400, 
-        "body": json.dumps([
-            {"deviceId": "a", "deviceType": "co2", "deviceName": "device1"}, 
-            {"deviceId": "b", "deviceType": "pir", "deviceName": "device2"}]), 
-        "headers": {'Access-Control-Allow-Origin': '*'} 
-    } 
-
+    print("")
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(DEVICES_TABLE)
+    print("")
+    print(table)
+    print("")
+    data = table.scan()
+    result = data["Items"]
+    print("")
+    print(result)
+    print("")
+    return {
+    "statusCode": 200,
+    "body": {"devices": json.dumps(result)},
+    "headers": {'Access-Control-Allow-Origin': '*'}
+  }
+    # return { 
+    #     "statusCode": 400, 
+    #     "body": json.dumps([
+    #         {"deviceId": "a", "deviceType": "co2", "deviceName": "device1"}, 
+    #         {"deviceId": "b", "deviceType": "pir", "deviceName": "device2"}]), 
+    #     "headers": {'Access-Control-Allow-Origin': '*'} 
+    # } 
 
